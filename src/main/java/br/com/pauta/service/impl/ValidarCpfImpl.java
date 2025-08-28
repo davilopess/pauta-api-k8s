0c428d236ad18bff540812b2815ce265bcc57fa1
davilopess <davi.lopes.amorim2014@hotmail.com>
2025-08-28 19:29:08 -0300

    Tratando exception

--- src/main/java/br/com/pauta/service/impl/ValidarCpfImpl.java
+++ src/main/java/br/com/pauta/service/impl/ValidarCpfImpl.java
@@ -1,21 +1,27 @@
 package br.com.pauta.service.impl;
 
 import br.com.pauta.dto.CpfResponseDTO;
-import br.com.pauta.exceptions.RestTemplateResponseErrorHandler;
+import br.com.pauta.exceptions.CpfUnableException;
 import br.com.pauta.service.ValidarCpf;
-import org.springframework.boot.web.client.RestTemplateBuilder;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @Service
 public class ValidarCpfImpl implements ValidarCpf {
     @Override
     public boolean validar(String cpf) {
-        String uri = "https://user-info.herokuapp.com/users/{cpf}";
-        RestTemplate restTemplate = new RestTemplate();
+        try{
+            String uri = "https://user-info.herokuapp.com/users/{cpf}";
+            RestTemplate restTemplate = new RestTemplate();
 
-       CpfResponseDTO result = restTemplate.getForObject(uri, CpfResponseDTO.class, cpf);
+            CpfResponseDTO result = restTemplate.getForObject(uri, CpfResponseDTO.class, cpf);
 
-        return result.getStatus().equals("ABLE_TO_VOTE") ? true : false;
+            return result.getStatus().equals("ABLE_TO_VOTE") ? true : false;
+        }catch (Exception e){
+            log.error("Erro ao fazer requisição externa para validação de CPF", e);
+            throw new CpfUnableException("Erro ao validar CPF");
+        }
     }
 }

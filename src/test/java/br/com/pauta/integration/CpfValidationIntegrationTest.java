ddedf2b9eb2027054c9686514abe446498145e06
davilopess <davi.lopes.amorim2014@hotmail.com>
2025-08-28 21:21:23 -0300

    Ajuste

---
+++ src/test/java/br/com/pauta/integration/CpfValidationIntegrationTest.java
@@ -0,0 +1,26 @@
+package br.com.pauta.integration;
+
+import br.com.pauta.exceptions.CpfUnableException;
+import br.com.pauta.service.ValidarCpf;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class CpfValidationIntegrationTest {
+    @Autowired
+    private ValidarCpf validarCpf;
+
+    @Test
+    void deveLancarExcecaoQuandoApiIndisponivel() {
+        String cpfTeste = "99999999999";
+
+        assertThrows(CpfUnableException.class, () -> {
+            validarCpf.validar(cpfTeste);
+        });
+    }
+}
